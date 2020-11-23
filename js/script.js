f631b036f6750cf84574656b3cfae34049af1536
san4us <66384085+san4us@users.noreply.github.com>
2020-11-23 19:33:18 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// ScriptProcessorNode
+let btnCreate = document.querySelector(".btn-create");
+let newElements = document.querySelector(".new-elements");
+let elem = document.createElement("p");
+
+elem.textContent = "Новый абзац";
+elem.classList.add("elem");
+
+btnCreate.onclick = function() {
+    newElements.append(elem);
+}
