7eabda1754c3c1511366a3d5d22a4c6ee70d9900
san4us <66384085+san4us@users.noreply.github.com>
2020-11-23 19:53:09 +0300

    3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-let btnCreate = document.querySelector(".btn-create");
-let newElements = document.querySelector(".new-elements");
-let elem = document.createElement("p");
+let btnCreate = document.querySelector('.btn-create');
+let newElements = document.querySelector('.new-elements');
+let elem;
+let numberElements = document.querySelector('.number-elements');
 
-elem.textContent = "Новый абзац";
-elem.classList.add("elem");
 
 btnCreate.onclick = function() {
-    newElements.append(elem);
+    for(let i = 0; i < +numberElements.value; i++) {
+        elem = document.createElement('p');
+        elem.textContent = "Новый абзац...";
+        elem.classList.add('elem');
+
+        newElements.append(elem);
+    }
 }
